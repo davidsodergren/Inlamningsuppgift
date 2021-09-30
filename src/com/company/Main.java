45423f3f66971dd850f44049d5d4d2a240e2a8b6
David Södergren <david.sodergren@pricer.com>
2021-09-30 14:56:39 +0200

    david commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,8 @@ public class Main {
         Student s = new Student("David");
 
 
-
-
-
-
+        System.out.println("blaa");
+        //This is a comment
         System.out.println("Person name: " + person.getName());
     }
 }
