802558a6d5f54c53194623f7fbf333f723630c61
davidsodergren <43667093+davidsodergren@users.noreply.github.com>
2021-09-30 15:13:43 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,13 @@ public class Main {
         Student s = new Student("David");
         System.out.println("blaa");
         //This is a comment
+        
+        
+        
+        
+        
+        
+        
         System.out.println("Person name: " + person.getName());
     }
 }
