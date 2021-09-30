920c0d3fb19700a6d37853571f22770d50c8ee22
David Södergren <david.sodergren@pricer.com>
2021-09-30 15:27:06 +0200

    SSM-1234: Removed comments and line in Main that caused system to crash

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Person person = new Person("David");
         Student s = new Student("David");
-        System.out.println("blaa");
-        //This is a comment
-        //ASDASDA
-        //ASDASDASD
-
         System.out.println("Person name: " + person.getName());
     }
 }
