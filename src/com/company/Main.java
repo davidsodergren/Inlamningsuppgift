5977eb4e7ced1b4c3cb616150a73732c2689ff66
David Södergren <david.sodergren@pricer.com>
2021-09-30 14:24:38 +0200

    My third commit. Removed "hello World" print

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
         Person person = new Person("David");
+
+        System.out.println("Person name: " + person.getName());
     }
 }
