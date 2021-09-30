f9e0828afb492c9f695e48d57e7fdd6d6e6c9493
David Södergren <david.sodergren@pricer.com>
2021-09-30 14:25:37 +0200

    My fourth commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Person person = new Person("David");
 
+
+
+
+
+
         System.out.println("Person name: " + person.getName());
     }
 }
