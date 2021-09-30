ca60cdeacb1df58cef70e89416dd952d2ebe06da
Philip Tonaczew <58476139+Tyareenbajka@users.noreply.github.com>
2021-09-30 14:55:37 +0200

    removed whitespace in class Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Person person = new Person("David");
-
         Student s = new Student("David");
-
-
-
-
-
-
         System.out.println("Person name: " + person.getName());
     }
 }
